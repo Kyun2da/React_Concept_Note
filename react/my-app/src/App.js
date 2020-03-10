da7c34b0ea04e6f2a5e8f375c180ae1ad473b0b1
Jeff Heo <jeff.heo@codingrobotlab.com>
2020-03-10 17:45:40 +0900

    [add] react 공부

---
+++ react/my-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import EventPractice from "./EventPractice";
+import EventPractice2 from "./EventPractice2";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
+import Counter from "./Counter";
+import Info from "./Info";
+function App() {
+  return (
+    <div className="App">
+      <EventPractice />
+      <EventPractice2 />
+      <ValidationSample />
+      {/* <ScrollBox ref={ref => (this.scrollBox = ref)} />
+      <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button> */}
+      <IterationSample />
+      <Counter />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
