2719889c8437c54aebc575b316c3d287825dbad3
Kyun Heo <kyun2dot@gmail.com>
2020-07-30 15:15:12 +0900

    feat: 리액트 블로그 회원 관련 기능 완료

--- reactPractice/blog/blog-frontend/src/containers/auth/RegisterForm.js
+++ reactPractice/blog/blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
@@ -6,6 +6,7 @@ import { check } from '../../modules/user';
 import { withRouter } from 'react-router-dom';
 
 const RegisterForm = ({ history }) => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
@@ -29,7 +30,18 @@ const RegisterForm = ({ history }) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
+    //하나라도 비어있다면
+    if ([username, password, passwordConfirm].includes('')) {
+      setError('빈 칸을 모두 입력하세요.');
+      return;
+    }
+    //비밀번호가 일치하지 않는다면
     if (password !== passwordConfirm) {
+      setError('비밀번호가 일치하지 않습니다.');
+      dispatch(changeField({ form: 'register', key: 'password', value: '' }));
+      dispatch(
+        changeField({ form: 'register', key: 'passwordConfirm', value: '' }),
+      );
       return;
     }
     dispatch(register({ username, password }));
@@ -43,8 +55,13 @@ const RegisterForm = ({ history }) => {
   //회원가입 성공/실패 처리
   useEffect(() => {
     if (authError) {
-      console.log('오류발생');
-      console.log(authError);
+      //계정명이 이미 존재할 때
+      if (authError.response.status === 409) {
+        setError('이미 존재하는 계정명입니다.');
+        return;
+      }
+      //기타 이유
+      setError('회원가입 실패');
       return;
     }
     if (auth) {
@@ -57,9 +74,12 @@ const RegisterForm = ({ history }) => {
   //user 값이 잘 설정되었는지 확인
   useEffect(() => {
     if (user) {
-      console.log('check API 성공');
-      console.log(user);
       history.push('/');
+      try {
+        localStorage.setItem('user', JSON.stringify(user));
+      } catch (e) {
+        console.log('localStorage is not working');
+      }
     }
   }, [history, user]);
   return (
@@ -68,6 +88,7 @@ const RegisterForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
