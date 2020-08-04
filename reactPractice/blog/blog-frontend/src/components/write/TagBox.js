e8669a5f99733cfa0a943adeacd76cfd3d250824
Kyun Heo <kyun2dot@gmail.com>
2020-08-04 17:09:55 +0900

    feat : 리액트 블로그 포스트 api 연동

--- reactPractice/blog/blog-frontend/src/components/write/TagBox.js
+++ reactPractice/blog/blog-frontend/src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -70,7 +70,7 @@ const TagList = React.memo(({ tags, onRemove }) => (
   </TagListBlock>
 ));
 
-const TagBox = () => {
+const TagBox = ({ tags, onChangeTags }) => {
   const [input, setInput] = useState('');
   const [localTags, setLocalTags] = useState([]);
 
@@ -78,16 +78,20 @@ const TagBox = () => {
     (tag) => {
       if (!tag) return; //공백이라면 추가하지 않음
       if (localTags.includes(tag)) return; //이미 존재한다면 추가하지 않음
-      setLocalTags([...localTags, tag]);
+      const nextTags = [...localTags, tag];
+      setLocalTags(nextTags);
+      onChangeTags(nextTags);
     },
-    [localTags],
+    [localTags, onChangeTags],
   );
 
   const onRemove = useCallback(
     (tag) => {
+      const nextTags = localTags.filter((t) => t !== tag);
       setLocalTags(localTags.filter((t) => t !== tag));
+      onChangeTags(nextTags);
     },
-    [localTags],
+    [localTags, onChangeTags],
   );
   const onChange = useCallback((e) => {
     setInput(e.target.value);
@@ -100,6 +104,11 @@ const TagBox = () => {
     },
     [input, insertTag],
   );
+
+  //tags 값이 바뀔 때
+  useEffect(() => {
+    setLocalTags(tags);
+  }, [tags]);
   return (
     <TagBoxBlock>
       <h4>태그</h4>
--- reactPractice/blog/blog-frontend/src/pages/WritePage.js
+++ reactPractice/blog/blog-frontend/src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
-import Editor from '../components/write/Editor';
-import TagBox from '../components/write/TagBox';
-import WriteActionButtons from '../components/write/WriteActionButtons';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
-      <Editor />
-      <TagBox />
-      <WriteActionButtons />
+      <EditorContainer />
+      <TagBoxContainer />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
