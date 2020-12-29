5c146bd0bb15bdc3231281bdcb4bd1ba4851d9d9
Kyun Heo <kyun2dot@gmail.com>
2020-12-29 18:07:42 +0900

    delete : 연습했던 폴더 삭제

--- reactPractice/blog/blog-backend/src/index.js
+++
@@ -1,5 +0,0 @@
-//이 파일에서만 해당 옵션을 비활성화 한다.
-/* eslint-disable no-global-assign */
-
-require = require('esm')(module /*,options*/);
-module.exports = require('./main.js');
--- reactPractice/blog/blog-frontend/src/lib/api/posts.js
+++
@@ -1,4 +0,0 @@
-import client from './client';
-
-export const writePost = ({ title, body, tags }) =>
-  client.post('/api/posts', { title, body, tags });
