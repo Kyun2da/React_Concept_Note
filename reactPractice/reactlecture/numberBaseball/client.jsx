293692451905560bd8bffff5045e4b8bee5d7ce7
Kyun Heo <kyun2dot@gmail.com>
2020-12-30 02:04:23 +0900

    feat: 숫자야구
    React.memo 및 PureComponent, Ref 사용법

---
+++ reactPractice/reactlecture/numberBaseball/client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import NumberBaseball from "./numberBaseballClass";
+
+ReactDOM.render(<NumberBaseball />, document.querySelector("#root"));
