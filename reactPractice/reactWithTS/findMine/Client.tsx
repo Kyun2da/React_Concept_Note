763fcae230b826178f37a472a556c4d6206864d4
Kyun Heo <kyun2dot@gmail.com>
2021-02-16 18:15:24 +0900

    feat : findMine with TS

---
+++ reactPractice/reactWithTS/findMine/Client.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import MineSearch from './MineSearch';
+
+ReactDOM.render(<MineSearch />, document.querySelector('#root'));
