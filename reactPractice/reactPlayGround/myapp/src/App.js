2423f92c6b0b94ce7cfcf5388fbb8861178c0bf5
Kyun Heo <kyun2dot@gmail.com>
2021-03-16 17:22:42 +0900

    feat : redux toolkit practice with ts

--- reactPractice/reactPlayGround/myapp/src/App.js
+++ reactPractice/reactPlayGround/myapp/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import KeyExample from './Pages/MapKey';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>홈으로 가기</Link>
+              </li>
+              <li>
+                <Link to='/mapkey'>맵키페이지로 가기</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path='/mapkey'>
+              <KeyExample />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ reactPractice/reactPlayGround/redux-toolkit-practice/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './features/counter/counter';
+
+const App = () => {
+  return (
+    <>
+      <Counter />
+    </>
+  );
+};
+
+export default App;
